cb458b1f264b51c81d2dd2cf147951bb95778bac
aibr24 <aliibrahim4793@gmail.com>
2020-07-09 17:57:33 +0300

    added the games list

---
+++ src/video-games.js
@@ -0,0 +1,67 @@
+const games = [
+  {
+    name: "FIFA2020 ",
+    genre: "Sports",
+    publisher: "EA Games",
+    imageUrl: "https://media.gamestop.com/i/gamestop/11094747/FIFA-20?$pdp$",
+    description:
+      "FIFA 20 is a football simulation video game published by Electronic Arts as part of the FIFA series",
+  },
+  {
+    name: "GTA ViceCity",
+    genre: "Action Adventure",
+    publisher: "Rockstar Games",
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/615H16JHVSL._SY445_.jpg",
+    description:
+      "Grand Theft Auto: Vice City is a 2002 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the first entry in the Grand Theft Auto series since 2001's Grand Theft Auto III. Set in 1986 within the fictional Vice City, based on Miami, the game follows mobster Tommy Vercetti following his release from prison. After he is caught up in an ambushed drug deal, he seeks out those responsible while building a criminal empire and seizing power from other criminal organisations in the city.",
+  },
+  {
+    name: "Call of Duty: Modern Warfare",
+    genre: "First Person Shooter",
+    publisher: "Blizzard/Activision",
+    imageUrl:
+      "https://cdn-products.eneba.com/resized-products/4FTGL0Ta9wnP4dqoBDEEE91a45upnnRDxUHIpKNwvIA_390x400_1x-0.jpeg",
+
+    description:
+      "Call of Duty is a first-person shooter video game franchise published by Activision.",
+  },
+  {
+    name: "Halo",
+    genre: "First Person Shooter",
+    publisher: "Bungie",
+    imageUrl:
+      "https://www.halopedia.org/images/thumb/2/27/Haloceanniversary.jpg/1200px-Haloceanniversary.jpg",
+    description:
+      "Halo is an American military science fiction media franchise centered on a series of video games managed and developed by 343 Industries, a division of Xbox Game Studios. Halo was originally created and developed by Bungie. The original trilogy centers on an interstellar war between humanity and an alliance of aliens known as the Covenant.",
+  },
+  {
+    name: "Path Of Exile",
+    genre: "Action RPG",
+    publisher: "GrindingGearGames",
+    imageUrl:
+      "https://www.mobygames.com/images/covers/l/551172-path-of-exile-playstation-4-front-cover.jpg",
+    description:
+      "You are an Exile, struggling to survive on the dark continent of Wraeclast, as you fight to earn power that will allow you to exact your revenge against those who wronged you. Created by hardcore gamers, Path of Exile is an online Action RPG set in a dark fantasy world. ",
+  },
+  {
+    name: "Fortnite",
+    genre: "Battle ROyale",
+    publisher: "Epic Games",
+    imageUrl:
+      "https://www.mobygames.com/images/covers/l/487051-fortnite-battle-royale-nintendo-switch-front-cover.jpg",
+    description:
+      "Fortnite is a survival game where 100 players fight against each other in player versus player combat to be the last one standing. It is a fast-paced, action-packed game, not unlike The Hunger Games, where strategic thinking is a must in order to survive.",
+  },
+  {
+    name: "World of Warcraft",
+    genre: "MMORPG",
+    publisher: "Blizzard/Activision",
+    imageUrl:
+      "https://giantbomb1.cbsistatic.com/uploads/square_medium/8/87790/2849278-box_wowtbc.png",
+    description:
+      "World of Warcraft (WoW) is a massively multiplayer online role-playing game (MMORPG) released in 2004 by Blizzard Entertainment. It is the fourth released game set in the Warcraft fantasy universe.[3] World of Warcraft takes place within the Warcraft world of Azeroth, approximately four years after the events at the conclusion of Blizzard's previous Warcraft release, Warcraft III: The Frozen Throne.[4] The game was announced in 2001, and was released for the 10th anniversary of the Warcraft franchise on November 23, 2004.",
+  },
+];
+
+export default games;
